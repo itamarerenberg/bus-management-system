d21f01a17ea905d59391f3fefe5dba8a310d5d68
itamar erenberg <ierenber@g.jct.ac.il>
2020-10-27 14:02:37 +0200

    start

---
+++ dotNet5781_00_1038_0685/dotNet5781_00_1038_0685/Program1038.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Dynamic;
+
+namespace dotNet5781_00_1038_0685
+{
+    partial class Program1038
+    {
+        static void Main(string[] args)
+        {
+            Welcom1038();
+            Console.ReadLine();
+        }
+
+        static partial void Welcom0685();
+        private static void Welcom1038()
+        {
+            Console.WriteLine("Enter your name: ");
+            string name = Console.ReadLine();
+            Console.WriteLine("{0}, welcome to my first consol application", name);
+        }
+    }
+}
