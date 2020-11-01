63b46c4ccadb5c1dfdb61a4fe41dbcab7e08d4cc
itamar erenberg <ierenber@g.jct.ac.il>
2020-11-01 13:33:06 +0200

    יצרתי פיתרון לתרגיל 1

---
+++ dotNet5781_01_1038_0685/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dotNet5781_01_1038_0685
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
