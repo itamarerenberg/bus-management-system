3621488df2c678b8f555b6d1562dc68e29c70342
msm770 <38422044+msm770@users.noreply.github.com>
2020-11-04 20:46:22 +0200

    class LineStation

---
+++ dotNet5781_00_1038_0685/dotNet5781_02_1038_0685/LineStation.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dotNet5781_02_1038_0685
+{
+    class LineStation : Station
+    {
+        //fields
+        public double Distance { get; set; }
+
+        public TimeSpan RideTime { get; set; }
+
+        //construcor
+        public LineStation(int code, double latiude, double longitude, double distance, TimeSpan rideTime, string address = "")
+            : base(code, latiude, longitude, address)
+        {
+            Distance = distance;
+            RideTime = rideTime;
+        }
+    }
+}
