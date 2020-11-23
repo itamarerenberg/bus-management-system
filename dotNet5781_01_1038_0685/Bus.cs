2c305f5cf1802be832f3b43789b0d131fbbbbd2b
itamar erenberg <ierenber@g.jct.ac.il>
2020-11-23 21:39:28 +0200

    I changed class Bus

--- dotNet5781_01_1038_0685/Bus.cs
+++ dotNet5781_01_1038_0685/Bus.cs
@@ -10,51 +10,100 @@ namespace dotNet5781_01_1038_0685
 {
     class Bus
     {
-        public string Id { get; private set; }
-        public DateTime Start_date { get; private set; }
-        public DateTime Last_treat_date { get; private set; }
-        public int Km { get; private set; }
-        public int Fuel { get; private set; }
-        public bool Danger { get; private set; }
+        #region fildes end properteys
 
-        //construcror
-        public Bus(string id, DateTime date, int km = 0, int fuel = 0)
-        {
-            Id = id;
-            Start_date = date;
-            Km = km;
-            Fuel = fuel;
-            Danger = false;
-        }
-
-        TimeSpan Time_dif(DateTime dt1, DateTime dt2)
-        {
-            int days = (dt1.Year - dt2.Year) * 365;//years difrence
-            days += (dt1.Month - dt2.Month) * 30;//Month dif
-            days += (dt1.Day - dt1.Day);
-            return new TimeSpan(days, 0, 0, 0);
-        }
-
-        public bool Ride(int km)
-        {
-            
-            if (!(Time_dif(Last_treat_date, DateTime.Now) > new TimeSpan(365, 0, 0, 0) || Km + km >= 20000) && Fuel >= km)
+        private int licensNum;/*filde*/
+        readonly DateTime StartDate;/*filde*/
+        public double Fule_in_km { get; private set; }/*property*/
+        public double General_km { get; private set; }/*property*/
+        public double Km { get; private set; }/*property*/
+        public DateTime lastTretDate { get; private set; }/*property*/
+        public int LicensNum/*property*/
+        { 
+            get => this.licensNum;
+            set 
             {
-                Km += km;
-                Fuel -= km;
-                return true;
+                if(this.StartDate >= new DateTime(2018,0,0))//if the StartTime is after or equals 1/1/2018 then confirm that the number length is 8
+                {
+                    if(licensNum.ToString().Length != 8)
+                    {
+                        throw new ArgumentException("the length of the licen's num must be suitible to the year of the bus");
+                    }
+                }
+
+                if (this.StartDate < new DateTime(2018, 0, 0))//if the StartTime is before 1/1/2018 then confirm that the number length is 7
+                {
+                    if (licensNum.ToString().Length != 7)
+                    {
+                        throw new ArgumentException("the length of the licen's num must be suitible to the year of the bus");
+                    }
+                }
+                this.licensNum = value;
             }
-            return false;
         }
-        public void Refuel()
+
+        #endregion
+
+        #region constractor
+
+        public Bus(int _licensNum, DateTime _startDate, int _km = 0, int _general_km = 0, DateTime _lastTretDate = new DateTime())
         {
-            Fuel = 1200;
+            this.licensNum = _licensNum;
+            this.StartDate = _startDate;
+            this.Km = _km;
+            this.General_km = _general_km;
+            this.lastTretDate = _lastTretDate;
         }
-        public void Treatment()
+
+        #endregion
+
+        #region methods
+
+        public bool ride(int km)
         {
-            Last_treat_date = DateTime.Now;
+            //check if the last treatment was less then one year
+            if(DateTime.Now - lastTretDate > new TimeSpan(365,0,0,0))
+            {
+                return false;
+            }
+
+            //check if this ride will cose to pass the 20,000km from last treatment
+            if(Km + km > 20000)
+            {
+                return false;
+            }
+
+            //check if ther is enough fule for the ride
+            if(Fule_in_km < km)
+            {
+                return false;
+            }
+
+            //update the km end the fule
+            Fule_in_km -= km;
+            Km += km;
+            General_km += km;
+
+            return true;
+        }
+
+        public void refule(int fule_in_km = 1200)
+        {
+            Fule_in_km += fule_in_km;
+        }
+
+        public void treatment()
+        {
+            lastTretDate = DateTime.Now;
             Km = 0;
-            Danger = false;
         }
+
+        public override string ToString()
+        {
+            return $"license's num: {LicensNum},    km: {Km}";
+        }
+
+        #endregion
+
     }
 }
