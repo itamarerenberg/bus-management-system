0265c542742fbaa8434c7b759a379ce8e8cf5350
itamarerenberg <59229663+itamarerenberg@users.noreply.github.com>
2020-12-06 19:49:37 +0200

    הכפתור של התדלוק מכובה כאשר הדלק על 1200

---
+++ dotNet5781_03b_1038_0685/Is_need_fule.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+
+namespace dotNet5781_03b_1038_0685
+{
+    class Is_need_fule : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if(!(value is double))
+            {
+                throw new ArgumentException("incorect value type");
+            }
+            double intvalue = (double)value;
+            if(intvalue >= 1200)
+            {
+                return false;
+            }
+            else
+            {
+                return true;
+            }
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
