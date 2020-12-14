1d153345ed0a1025c7eba33beb4bb0fa5518a912
msm770 <38422044+msm770@users.noreply.github.com>
2020-12-14 18:29:30 +0200

    BusCollection: code documentation

--- dotNet5781_03b_1038_0685/busCollection.cs
+++ dotNet5781_03b_1038_0685/busCollection.cs
@@ -11,13 +11,16 @@ namespace dotNet5781_03b_1038_0685
 {
     public class busCollection : INotifyPropertyChanged
     {
-        private ObservableCollection<Bus> buses = new ObservableCollection<Bus>();
+        #region private fields
+        private ObservableCollection<Bus> buses;
         private List<Bus> busesList;
         private string readyBussesMsg;
         private string busyBussesMsg;
         private string needTreatBussesMsg;
         private string totalBaussesMsg;
+        #endregion
 
+        #region properties
         public ObservableCollection<Bus> Buses
         {
             get => buses;
@@ -38,6 +41,9 @@ namespace dotNet5781_03b_1038_0685
                 PropertyChanged?.Invoke(this, new PropertyChangedEventArgs("BusesList"));
             }
         }
+        /// <summary>
+        /// string maessage: how many busses are ready for drive
+        /// </summary>
         public string ReadyBussesMsg
         {
             get => readyBussesMsg;
@@ -47,13 +53,21 @@ namespace dotNet5781_03b_1038_0685
                 PropertyChanged?.Invoke(this, new PropertyChangedEventArgs("ReadyBussesMsg"));
             }
         }
-        public string BusyBussesMsg { get => busyBussesMsg;
+        /// <summary>
+        /// string maessage: how many busses are busy
+        /// </summary>
+        public string BusyBussesMsg
+        {
+            get => busyBussesMsg;
             set
             {
                 busyBussesMsg = value;
                 PropertyChanged?.Invoke(this, new PropertyChangedEventArgs("BusyBussesMsg"));
             }
         }
+        /// <summary>
+        /// string maessage: how many busses need treatment
+        /// </summary>
         public string NeedTreatBussesMsg
         {
             get => needTreatBussesMsg;
@@ -63,20 +77,26 @@ namespace dotNet5781_03b_1038_0685
                 PropertyChanged?.Invoke(this, new PropertyChangedEventArgs("NeedTreatBussesMsg"));
             }
         }
-        public string TotalBaussesMsg { get => totalBaussesMsg;
+        /// <summary>
+        /// string maessage: how many busses in this collection
+        /// </summary>
+        public string TotalBaussesMsg
+        {
+            get => totalBaussesMsg;
             set
             {
                 totalBaussesMsg = value;
                 PropertyChanged?.Invoke(this, new PropertyChangedEventArgs("TotalBaussesMsg"));
             }
-        }
+        } 
+        #endregion
 
-        public event PropertyChangedEventHandler PropertyChanged;
         #region constractor
         public busCollection(ObservableCollection<Bus> list)
         {
             Buses = list;
             Buses.CollectionChanged += Buses_CollectionChanged;
+            //register each PropertyChangedEvent of Bus to the item_PropertyChanged Handler
             foreach (Object item in Buses)
             {
                 (item as INotifyPropertyChanged).PropertyChanged += new PropertyChangedEventHandler(item_PropertyChanged);
@@ -85,10 +105,21 @@ namespace dotNet5781_03b_1038_0685
         #endregion
 
         #region events handeling
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        /// <summary>
+        /// occur when adding a bus to the collection.
+        /// actions:  
+        /// 1. register the PropertyChangedEvent of the new Bus to the item_PropertyChanged Handler
+        /// 2. update the list
+        /// 3. update all messages
+        /// </summary>
         private void Buses_CollectionChanged(object sender, NotifyCollectionChangedEventArgs e)
         {
             if (e.NewItems != null)
             {
+                //register the PropertyChangedEvent of the new Bus to the item_PropertyChanged Handler
                 foreach (Object item in e.NewItems)
                 {
                     (item as INotifyPropertyChanged).PropertyChanged += new PropertyChangedEventHandler(item_PropertyChanged);
@@ -99,14 +130,18 @@ namespace dotNet5781_03b_1038_0685
             }
         }
 
+        /// <summary>
+        /// occur when Property of any Bus in the collection is changed.
+        /// </summary>
         private void item_PropertyChanged(object sender, PropertyChangedEventArgs e)
         {
             update_messages();
         }
 
+
         private void update_messages()
         {
-            int num = BusesList.FindAll(b => b.Stat == StatEnum.READY).Count();
+            int num = BusesList.FindAll(b => b.Stat == StatEnum.READY).Count();//number of busses with the status READY
             switch (num)
             {
                 case 0:
@@ -119,8 +154,9 @@ namespace dotNet5781_03b_1038_0685
                     ReadyBussesMsg = $"{num} busses are ready for driving";
                     break;
             }
+
             num = BusesList.FindAll(b => b.Stat == StatEnum.IN_REFUELING ||
-            b.Stat == StatEnum.IN_TREATMENT || b.Stat == StatEnum.IS_TRAVELING).Count();
+            b.Stat == StatEnum.IN_TREATMENT || b.Stat == StatEnum.IS_TRAVELING).Count();//number of busses with the status IN_REFUELING or IN_TREATMENT
             switch (num)
             {
                 case 0:
@@ -133,7 +169,7 @@ namespace dotNet5781_03b_1038_0685
                     BusyBussesMsg = $"{num} busses are busy";
                     break;
             }
-            num = BusesList.FindAll(b => b.Stat == StatEnum.NEED_TREATMENT).Count();
+            num = BusesList.FindAll(b => b.Stat == StatEnum.NEED_TREATMENT).Count();// number of busses with the status NEED_TREATMENT
             switch (num)
             {
                 case 0:
